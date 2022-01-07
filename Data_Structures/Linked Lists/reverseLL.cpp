e93a306fc9c233e9f04f48279c66ae3974895f3c
Harrshaa <sriharsha.mutakodoor@gmail.com>
2022-01-07 12:36:19 +0530

    initial commit

---
+++ Data_Structures/Linked Lists/reverseLL.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Sllnode
+{
+  int data;
+  Sllnode *next;
+  Sllnode(int data)
+  {
+    this->data=data;
+    this->next=nullptr;
+  }
+};
+class Sll
+{
+  Sllnode *head;
+  Sll()
+  {
+    this->head=nullptr;
+  }
+
+};
+
+void display(Sllnode *head)
+{
+    
+}
+
+int main()
+{
+
+
+  
+}
